9ac72ff5cf74edad16e58f62a64ed893476ec5ad
fault <critical@fault.io>
2021-06-04 12:50:03 -0700

    Add some structure to the functionality and avoid execution outside of __main__.

--- pyx.py
+++ pyx.py
@@ -1,12 +1,28 @@
+"""
+# Python factor execution script used in the absence of system bindings.
+
+# Normally, this is only executed by &.setup, but in cases where &..python.bin.bind
+# cannot create usable executables with the system's compiler driver, shell scripts
+# will be created instead invoking this script with the necessary environment variables.
+"""
 import sys
 import os
 import os.path
 import importlib
 
-# Use sys.path entries and bootstrapped (python.sh) extension modules.
-sys.path.append(os.path.dirname(os.environ['FAULT_PYTHON_PATH']))
-sys.path.append(os.path.dirname(os.environ['FAULT_SYSTEM_PATH']))
+# Environment variables normally set by &..root.setup.
+path_sources = ['FAULT_PYTHON_PATH', 'FAULT_SYSTEM_PATH']
+fault_name = os.environ['FAULT_CONTEXT_NAME']
+subexec_module_path = '.'.join((fault_name, 'system', 'bin', 'subexec'))
 
-module_path = '.'.join((os.environ['FAULT_CONTEXT_NAME'], 'system', 'bin', 'subexec'))
-subexec = importlib.import_module(module_path)
-subexec.process.control(subexec.main, subexec.process.Invocation.system())
+def extend_python_path(pathrefs):
+	# Use sys.path entries and bootstrapped (python.sh) extension modules.
+	sys.path.extend(map(os.path.dirname, (os.environ[x] for x in pathrefs)))
+
+def av_execution(module_path):
+	extend_python_path(path_sources)
+	subexec = importlib.import_module(module_path)
+	subexec.process.control(subexec.main, subexec.process.Invocation.system())
+
+if __name__ == '__main__':
+	av_execution(subexec_module_path)
