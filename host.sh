ca3e860ac6c02103ad9e27c388752521471a9f06
fault <critical@fault.io>
2021-06-06 17:13:45 -0700

    Bind user addressable tools into ./bin and rename tool to fault-dispatch for internal subprocesses.

--- host.sh
+++ host.sh
@@ -1,10 +1,8 @@
 # Initialize the host platform.
 PLATFORM="$1"; shift 1
+CCONTEXT="$PLATFORM/cc/optimal"
 
-pyx system.platforms.bin.initialize "$PLATFORM" || exit
-CONTEXT="$PLATFORM/cc/optimal"
-export CONTEXT
-
-pyx system.factors.bin.initialize optimal "$CONTEXT" || exit
-pyx system.machine.bin.setup CC "$(which cc)" || exit
-pyx system.python.bin.setup executable "$FAULT_LIBEXEC_PATH/compile-python" || exit
+fault-tool python system.platforms.bin.initialize "$PLATFORM" || exit
+fault-tool python system.factors.bin.initialize optimal "$CCONTEXT" || exit
+fault-tool python system.machine.bin.setup "$CCONTEXT" CC "$(which cc)" || exit
+fault-tool python system.python.bin.setup "$CCONTEXT" || exit
--- libexec.sh
+++ libexec.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
-# Populate the (system/environ)`FAULT_LIBEXEC_PATH` directory with executable bindings.
-# Presumes &system.root.parameters has been sourced.
+# Populate (system/environ)`FAULT_LIBEXEC_PATH` and (system/environ)`FAULT_TOOL_PATH` directories
+# with executable bindings. Presumes &system.root.parameters has been sourced.
+SYSTEM_PRODUCT="$(cd "$FAULT_SYSTEM_PATH/.." && pwd)"
 
 "$PYTHON" "$PYX" .module system.python.bin.bind \
-	"-F$FAULT_PYTHON_PATH" \
-	"-L$(cd "$FAULT_SYSTEM_PATH/.." && pwd)" \
+	"-F$FAULT_PYTHON_PATH" "-L$SYSTEM_PRODUCT" \
+	"$FAULT_TOOL_PATH/pdctl" "system.products.bin.control" || exit
+
+"$PYTHON" "$PYX" .module system.python.bin.bind \
+	"-F$FAULT_PYTHON_PATH" "-L$SYSTEM_PRODUCT" \
 	"-lfault.context.bin.tools" \
 	"-lsystem.context.bin.tools" \
-	"$FAULT_LIBEXEC_PATH/pyx" "fault.system.bin.tool" || exit
+	"$FAULT_TOOL_PATH/fault-tool" "fault.system.bin.tool" || exit
 
-(cd "$FAULT_LIBEXEC_PATH" || exit
-	ln -f pyx http-cache
-	ln -f pyx factors-cc
-	ln -f pyx compile-python
-	ln -f pyx delineate-python
-	ln -f pyx pdctl)
+"$PYTHON" "$PYX" .module system.python.bin.bind \
+	"-F$FAULT_PYTHON_PATH" "-L$SYSTEM_PRODUCT" \
+	"-lfault.context.bin.tools" \
+	"-lsystem.context.bin.tools" \
+	"$FAULT_LIBEXEC_PATH/fault-dispatch" "fault.system.bin.tool" || exit
