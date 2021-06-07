b8a0c6decf5095d1e7d1e53699661bc27a7cdf98
fault <critical@fault.io>
2021-06-06 17:05:47 -0700

    Rely on parameters.sh for python location information.

--- python.sh
+++ python.sh
@@ -1,30 +1,9 @@
 #!/bin/sh
 # Create the system process extensions necessary to initialize and execute construction contexts.
 # Presumes &system.root.parameters has been sourced.
-SCD=`pwd`
-if readlink "$PYTHON"
-then
-	cd "$(dirname "$PYTHON")"
-	pylink="$(readlink "$PYTHON")"
-	cd "$(dirname "$pylink")"
-	cd ..
-	prefix="$(pwd)"
-else
-	prefix="$(dirname "$(dirname "$PYTHON")")"
-fi
-cd "$SCD"
-unset SCD
 
-evalpy () { "$PYTHON" -c "$1"; }
-pyversion="$(evalpy 'import sys; print(".".join(map(str, sys.version_info[:2])))')"
-pyabi="$(evalpy 'import sys; print(sys.abiflags)')"
-pytype="$(evalpy 'import sys; print(sys.implementation.name)')"
-
-test $? -eq 0 || exit 1
-
-echo "ABI: $pyabi"
-echo "VERSION: $pyversion"
-echo "TYPE: $pytype"
+prefix="$PYTHON_PREFIX"
+pylib="python$PYTHON_VERSION$PYTHON_ABI"
 
 compile ()
 {
@@ -39,22 +18,19 @@ platsuffix="so" # Following platform switch overrides when necessary.
 
 case "$defsys" in
 	*darwin*)
-		osflags="-Wl,-bundle,-undefined,dynamic_lookup,-lSystem,-L$prefix/lib,-lpython$pyversion$pyabi -fPIC";
+		osflags="-Wl,-bundle,-undefined,dynamic_lookup,-lSystem,-L$prefix/lib,-l$pylib -fPIC";
 	;;
 	*freebsd*)
-		osflags="-Wl,-lc,-L$prefix/lib,-lpython$pyversion$pyabi -fPIC -shared -pthread"
+		osflags="-Wl,-lc,-L$prefix/lib,-l$pylib -fPIC -shared -pthread"
 	;;
 	*)
-		osflags="-Wl,-shared,--export-all-symbols,--export-dynamic,-lc,-lpthread,-L$prefix/lib,-lpython$pyversion$pyabi -fPIC"
+		osflags="-Wl,-shared,--export-all-symbols,--export-dynamic,-lc,-lpthread,-L$prefix/lib,-l$pylib -fPIC"
 	;;
 esac
-
-original="$(pwd)"
 
 cd "$FAULT_PYTHON_PATH"
 fault_dir="$(pwd)"
 container_dir="$(dirname "$fault_dir")"
-echo $container_dir
 
 module_path ()
 {
@@ -88,7 +64,7 @@ bootstrap_extension ()
 		"-I$FAULT_SYSTEM_PATH/machine/include/src" \
 		"-I$fault_dir/system/include/src" \
 		"-I$prefix/include" \
-		"-I$prefix/include/python$pyversion$pyabi" \
+		"-I$PYTHON_INCLUDE" \
 		"-DF_SYSTEM=$defsys" \
 		"-DF_TARGET_ARCHITECTURE=$defarch" \
 		"-DF_INTENTION=debug" \
@@ -107,8 +83,6 @@ bootstrap_project ()
 	cd "$1"
 	root="$(dirname "$(pwd)")"
 
-	echo $root
-	echo $(pwd)
 	# Nothing to do?
 	test -d ./extensions || return 0
 
