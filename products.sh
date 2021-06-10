3b7ae916aa392254412856750c891d4dd1020b87
fault <critical@fault.io>
2021-06-10 09:29:48 -0700

    Disable product index updates; performed earlier in setup.

--- products.sh
+++ products.sh
@@ -1,9 +1,9 @@
 CCONTEXT="$1"; shift 1
 (cd "$FAULT_PYTHON_PATH/.." || exit
 	pdctl connect "$FAULT_INSTALLATION_PATH/integration"
-	pdctl -X "$CCONTEXT" integrate "$FAULT_CONTEXT_NAME" "$@") || exit
+	pdctl -x "$CCONTEXT" integrate -u "$FAULT_CONTEXT_NAME" "$@") || exit
 (cd "$FAULT_SYSTEM_PATH/.." || exit
 	pdctl connect "$FAULT_INSTALLATION_PATH/python"
-	pdctl -X "$CCONTEXT" integrate system "$@") || exit
+	pdctl -x "$CCONTEXT" integrate -u system "$@") || exit
 
 libexec.sh "optimal"
